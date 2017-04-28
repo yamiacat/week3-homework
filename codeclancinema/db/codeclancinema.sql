7f2eddf17a71f0e7b6cc6bd4d2bf2dc11445b76f
Matt Rose <yamiacat@gmail.com>
2017-04-28 12:49:21 +0100

    Add sql file

---
+++ codeclancinema/db/codeclancinema.sql
@@ -0,0 +1,31 @@
+DROP TABLE IF EXISTS tickets;
+DROP TABLE IF EXISTS screenings;
+DROP TABLE IF EXISTS films;
+DROP TABLE IF EXISTS customers;
+
+
+CREATE TABLE customers (
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255),
+  funds INT4
+);
+
+CREATE TABLE films (
+  id SERIAL4 PRIMARY KEY,
+  title VARCHAR(255),
+  certificate VARCHAR(255)
+);
+
+CREATE TABLE screenings (
+  id SERIAL4 PRIMARY KEY,
+  screening_date DATE,
+  screening_time VARCHAR(255),
+  screening_film INT4 REFERENCES films(id) ON DELETE CASCADE
+);
+
+CREATE TABLE tickets (
+  id SERIAL4 PRIMARY KEY,
+  customer_id INT4 REFERENCES customers(id) ON DELETE CASCADE,
+  film_id INT4 REFERENCES films(id) ON DELETE CASCADE,
+  screening_id INT4 REFERENCES screenings(id) ON DELETE CASCADE
+);
